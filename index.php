38c8fe977d7ef876c6b791c5b60c07f287ce9277
Jennie Holm <jennie.holm@optistud.fi>
2018-02-02 13:52:05 +0200

    made splashScreen

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 $bodyClass = "d-flex justify-content-center align-items-center";
 include 'includes/header.php';
+
+$query = "SELECT * FROM users";
+$result = mysqli_query($connection, $query);
+$num_rows = mysqli_num_rows($result);
  ?>
 
 
@@ -11,7 +15,11 @@ include 'includes/header.php';
 
 
 <main class="animated fadeInDown">
+  <h1>List</h1>
   <img src="img/listLogo.svg" class="img-fluid" alt="List">
+  <p> An online to-do list with <?php echo $num_rows;  ?> members! </p>
+  <a class="button" href="login.php">Log in</a>
+  <a class="button" href="register.php">Register</a>
 </main>
 
 
